c5702d8c34a4d2450b4f6222a8d66cb4cd80059f
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-24 11:38:31 +0100

    wip adding journalct

--- src/index.js
+++ src/index.js
@@ -7,17 +7,35 @@ const exec = util.promisify(child_process.exec);
 const PLATFORM_SUPPORTED = [ 'linux' ];
 const platform = os.platform();
 
-export async function jlogger(message, tag) {
+function checkPlatform() {
     if (!PLATFORM_SUPPORTED.includes(platform)) {
         throw new Error(`${platform} is not supported, ` +
             `you need a ${PLATFORM_SUPPORTED}`);
     }
+}
+
+export async function jlogger(message, tag) {
+    checkPlatform();    
     if (typeof message === 'undefined' || !message.trim().length) {
         throw new Error('\'message\' parameter is mandatory');
     }
     let loggerCommand = `logger ${message}`;
     if (typeof tag !== 'undefined') {
-        loggerCommand = `${loggerCommand} --tag ${tag} `;
+        loggerCommand = `${loggerCommand} --tag ${tag}`;
     }
     return await exec(loggerCommand);
 }
+
+/* eslint no-console: "off" */
+export async function jjournal(tag) {
+    checkPlatform();
+    if (typeof tag === 'undefined' || !tag.trim().length) {
+        throw new Error('\'tag\' parameter is mandatory');
+    }
+    const journalctlCommand = `journalctl -t ${tag}`;
+    const { stdout, stderr } = await exec(journalctlCommand);
+    console.log(typeof stdout);
+    console.log(typeof stderr);
+    return stdout;
+}
+
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,36 +1,75 @@
 /* eslint no-console: "off" */
 
 import test from 'ava';
-import { jlogger } from '../src/index.js';
+import { jlogger, jjournal } from '../src/index.js';
+
+const MESSAGE = 'a foo in tha bar for lol';
+const TAG = 'NODE_SYSTEMD_JOURNAL';
 
 test('should create an entry at systemd journal', async(t) => {
-    await jlogger('should create an entry at systemd journal');
+    await jlogger('an entry at systemd journal without tag');
     t.pass();
 });
 
 test('should throws an error', async(t) => {
-    try {     
+    await t.throwsAsync(async() => {
         await jlogger('`');
-    } catch (err) {
-        t.true(err instanceof Error);
-    }
+    }, { instanceOf: Error });
 });
 
-test('should create an entry at systemd journal with a tag', async(t) => {
-    await jlogger('should create an entry at systemd journal',
-        'NODE_SYSTEMD_JOURNAL');
+test.serial('should create an entry at systemd journal with a tag', async(t) => {
+    await jlogger(MESSAGE, TAG); 
     t.pass();
 });
 
-test('should throw an error since there is no tag', async(t) => {
-    try {
-        //await jlogger('should create an entry at systemd journal','');
-        await jlogger(undefined);
-    } catch (err) {
-        console.log(err.message);
-        t.true(err instanceof Error);
-    }
+test('should throw an error since there is no message', async(t) => {
+    await t.throwsAsync(async() => {
+        await jlogger(' ');
+    }, { instanceOf: Error, message: '\'message\' parameter is mandatory' });
 });
+
+test('should throw an error since message is empty', async(t) => {
+    await t.throwsAsync(async() => {
+        await jlogger(' ');
+    }, { instanceOf: Error, message: '\'message\' parameter is mandatory' });
+});
+
+test('should throw an error since message consist of spaces', async(t) => {
+    await t.throwsAsync(async() => {
+        await jlogger(' ');
+    }, { instanceOf: Error, message: '\'message\' parameter is mandatory' });
+});
+
+test.serial('should retrieve a log by tag', async(t) => {
+    const slog = await jjournal(TAG);
+    t.true(slog.indexOf(MESSAGE) >= 0);
+});
+
+test('jjournal should throw an error', async(t) => {
+    await t.throwsAsync(async() => {
+        await jjournal('`');
+    }, { instanceOf: Error });
+});
+
+test('should not retrieve a log since tag does not exists', async(t) => {
+    await t.throwsAsync(async() => {
+        await jjournal(undefined);
+    }, { instanceOf: Error, message: '\'tag\' parameter is mandatory' });
+});
+
+test('should not retrieve a log since tag is empty', async(t) => {
+    await t.throwsAsync(async() => {
+        await jjournal('');
+    }, { instanceOf: Error, message: '\'tag\' parameter is mandatory' });
+});
+
+test('should not retrieve a log since tag consists of spaces', async(t) => {
+    await t.throwsAsync(async() => {
+        await jjournal(' ');
+    }, { instanceOf: Error, message: '\'tag\' parameter is mandatory' });
+});
+
+
 // test('should retrieve a random face', (t) => {
 // console.log(face);
 
