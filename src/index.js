879f43941c76366facc3d8fab9552f344a7e7efc
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-28 13:29:16 +0100

    converted to a class

--- src/index.js
+++ src/index.js
@@ -5,41 +5,63 @@ import { toJSONArray, checkPlatform } from './util.js';
 
 const exec = util.promisify(child_process.exec);
 
-export async function jlogger(message, tag) {
-    checkPlatform();
-    if (typeof message === 'undefined' || !message.trim().length) {
-        throw new Error('\'message\' parameter is mandatory');
-    }
-    let loggerCommand = `logger ${message}`;
-    if (typeof tag !== 'undefined') {
-        loggerCommand = `${loggerCommand} --tag ${tag}`;
-    }
-    return await exec(loggerCommand);
-}
+export default class SysdJournal {
+    options = {
+        tag: this.constructor.name,
+        json: false,
+        lines: 0,
+        reverse: false,
+    };
 
-export async function jjournal(tag, {
-    json = false,
-    lines = 0,
-    reverse = false
-} = {}) {
-    checkPlatform();
-    if (typeof tag === 'undefined' || !tag.trim().length) {
-        throw new Error('\'tag\' parameter is mandatory');
+    constructor(opts) {
+        checkPlatform();
+        const options = opts || {};
+        if (typeof options.tag === 'undefined' || !options.tag.trim().length) {
+            options.tag = this.options.tag;
+        }
+        this.options = options || this.options;
     }
-    let journalctlCommand = `journalctl -t ${tag}`;
-    if (json) {
-        journalctlCommand = `${journalctlCommand} -o json`;
-    }
-    if (lines) {
-        journalctlCommand = `${journalctlCommand} -n ${lines}`;
-    }
-    if (reverse) {
-        journalctlCommand = `${journalctlCommand} -r`;
-    }
-    const { stdout } = await exec(journalctlCommand);
-    if (json) {
-        return toJSONArray(stdout);
-    }
-    return stdout;
-}
 
+    #getJournalCommand(tag, json, lines, reverse) {
+        const t = tag || this.options.tag;
+        const line = lines || this.options.lines;
+
+        let journalctlCommand = `journalctl -t ${t}`;
+        if (json || this.options.json) {
+            journalctlCommand = `${journalctlCommand} -o json`;
+        }
+        if (line) {
+            journalctlCommand = `${journalctlCommand} -n ${line}`;
+        }
+        if (reverse || this.options.reverse) {
+            journalctlCommand = `${journalctlCommand} -r`;
+        }
+        return journalctlCommand;
+    }
+
+    async write(message, { tag = undefined } = {}) {
+        if (typeof message === 'undefined' ||
+            typeof message !== 'string' || !message.trim().length) {
+            throw new Error('\'message\' parameter is mandatory and ' +
+                'must be a string');
+        }
+        let loggerCommand = `logger ${message}`;
+        const t = tag || this.options.tag;
+        loggerCommand = `${loggerCommand} --tag ${t}`;
+        return await exec(loggerCommand);
+    }
+
+    async read({
+        tag = undefined,
+        json = false,
+        lines = 0,
+        reverse = false
+    } = {}) {
+        const command = this.#getJournalCommand(tag, json, lines, reverse);
+        const { stdout } = await exec(command);
+        if (json || this.options.json) {
+            return toJSONArray(stdout);
+        }
+        return stdout;
+    }
+}
